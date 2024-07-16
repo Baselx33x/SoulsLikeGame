10b08d51c4afc926d629c18942345a804f32680d
BaselX <Basel.x33x@gmail.com>
2024-07-16 12:16:46 +0200

    Base `Iteam C-- Class
    1. Created a Base for the Any Iteam on the Game to Inherit From.

---
+++ Source/SoulsLikeGame/Iteams/Iteam.cpp
@@ -0,0 +1,62 @@
+
+
+#include "Iteam.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+
+AIteam::AIteam()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Init(); 
+	
+}
+
+
+void AIteam::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void AIteam::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AIteam::BeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (GEngine)
+	{
+		FString ActorName ="Overlapped With  : " + OtherActor->GetName();
+		GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, ActorName);
+	}
+}
+
+void AIteam::EndOverLap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (GEngine)
+	{
+		FString ActorName = "End Overlapped With  : " + OtherActor->GetName();
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Blue, ActorName);
+	}
+}
+
+void AIteam::Init()
+{
+	m_SphereCollision = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollision"));
+	m_SphereCollision->SetupAttachment(RootComponent);
+
+	m_IteamMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("IteamMesh"));
+	m_IteamMesh->SetupAttachment(m_SphereCollision);
+
+
+	m_SphereCollision->OnComponentBeginOverlap.AddDynamic(this, &AIteam::BeginOverlap);
+	m_SphereCollision->OnComponentEndOverlap.AddDynamic(this, &AIteam::EndOverLap);
+
+
+}
+
---
+++ Source/SoulsLikeGame/Iteams/Iteam.h
@@ -0,0 +1,54 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Iteam.generated.h"
+
+class UStaticMeshComponent;
+class USphereComponent;
+
+UCLASS()
+class SOULSLIKEGAME_API AIteam : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	AIteam();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+ UFUNCTION(BlueprintCallable)
+
+ virtual void BeginOverlap(UPrimitiveComponent* OverlappedComponent
+	 , AActor* OtherActor
+	 , UPrimitiveComponent* OtherComp
+	 , int32 OtherBodyIndex
+	 , bool bFromSweep
+	 , const FHitResult& SweepResult);
+
+ UFUNCTION(BlueprintCallable)
+ virtual void EndOverLap(UPrimitiveComponent* OverlappedComponent
+	 , AActor* OtherActor
+	 , UPrimitiveComponent* OtherComp
+	 , int32 OtherBodyIndex);
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Inisilization")
+	UStaticMeshComponent* m_IteamMesh = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Inisilization")
+	USphereComponent* m_SphereCollision = nullptr;
+
+private: 
+
+	void Init(); 
+
+
+};
