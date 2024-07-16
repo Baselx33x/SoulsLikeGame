662a21bba855b09500cda149792b757871a90c86
BaselX <Basel.x33x@gmail.com>
2024-07-16 15:46:02 +0200

    Save LVL

--- Source/SoulsLikeGame/Iteams/Weapon/Axe.cpp
+++ Source/SoulsLikeGame/Iteams/Weapon/Axe.cpp
@@ -9,16 +9,10 @@ void AAxe::BeginOverlap(UPrimitiveComponent* OverlappedComponent
 	, const FHitResult& SweepResult)
 {
 
-	m_B_IsCollided = true;
-
-	
-	
 	if (GEngine)
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Red, TEXT(" Axe BeginOverlap"));
 	}
-
-	
 }
 
 void AAxe::EndOverLap(UPrimitiveComponent* OverlappedComponent
@@ -26,25 +20,16 @@ void AAxe::EndOverLap(UPrimitiveComponent* OverlappedComponent
 	, UPrimitiveComponent* OtherComp
 	, int32 OtherBodyIndex)
 {
-	m_B_IsCollided = false;
+
 	if (GEngine)
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Blue, TEXT(" Axe EndOverLap"));
 	}
 
-
 }
 
 void AAxe::PickUP()
 {
-	if (m_B_IsCollided) {
-		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Black, TEXT(" True"));
-	}
-	else
-	{
-		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Black, TEXT(" False"));
-	}
-	
 	if (GEngine)
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Yellow, TEXT(" Axe PickUP"));
