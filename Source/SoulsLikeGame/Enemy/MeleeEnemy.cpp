70d8959d3ccf6aa9b0b8c04c27d1f32319849912
BaselX <Basel.x33x@gmail.com>
2024-07-19 10:58:13 +0200

    Determine the Hit Direction and Doing the Animation for it
    1. Under Get Hit Enemy will Determine where did he get hit and then make the appropriate animation for it.

--- Source/SoulsLikeGame/Enemy/MeleeEnemy.cpp
+++ Source/SoulsLikeGame/Enemy/MeleeEnemy.cpp
@@ -8,13 +8,62 @@ ABaseEnemy* AMeleeEnemy::GetHit(const FVector& HitPostion)
 	if (AnimInstce && m_GetHitMontage)
 	{
 
-		//FString SectionToPlay =""
+		FString SectionToPlay = ""; 
+
+		const FVector& ForwardVector = GetActorForwardVector();
+		const FVector& Direction = (HitPostion - GetActorLocation()).GetSafeNormal();
+
+		float Dot = FVector::DotProduct(ForwardVector, Direction);
+		float ClampedDot = FMath::Clamp(Dot, -1.f, 1.f);
+
+		float Angle = FMath::RadiansToDegrees(FMath::Acos(ClampedDot));
+		float sign = FVector::CrossProduct(ForwardVector, Direction).Z;
+		sign = sign > 0 ? 1 : -1;
+		
+		Angle *= sign;
+
+		if (Angle < 45.f && Angle > -45.f)
+		{
+			SectionToPlay = "GetFromHitFront";
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Cyan, TEXT("GetFromHitFront"));
+			}
+		}
+		else if (Angle < -45.f && Angle > -135.f)
+		{
+			SectionToPlay = "GetFromHitLeft";
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Cyan, TEXT("GetFromHitLeft"));
+			}
+		}
+		else if (Angle < -135.f && Angle > 135.f)
+		{
+			SectionToPlay = "GetFromHitBack";
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Cyan, TEXT("GetFromHitBack"));
+			}
+		}
+		else if (Angle < 135.f && Angle > 45.f)
+		{
+			SectionToPlay = "GetFromHitRight";
+			if (GEngine)
+			{
+				GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Cyan, TEXT("GetFromHitRight"));
+			}
+		}
+
+
+
 		if (GEngine)
 		{
 			GEngine->AddOnScreenDebugMessage(1,1.f,FColor::Cyan,TEXT("Geting Hit !"));
 		}
 
 		AnimInstce->Montage_Play(m_GetHitMontage);
+		AnimInstce->Montage_JumpToSection(FName(SectionToPlay), m_GetHitMontage);
 
 	}
 
